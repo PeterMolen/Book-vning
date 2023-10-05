dfcd6e5c075d2a85e11a11fb4372c9a080f03cb9
PeterMolen <144238673+PeterMolen@users.noreply.github.com>
2023-10-05 14:05:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BookÖvning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Book myBook = new Book("Title", "Author", "Summary of the book");
+            myBook.PrintBookData();
+
+
+            Console.Write("Input Title: ");
+            myBook._title = Console.ReadLine();
+            Console.Write("Input Author: ");
+            myBook._author = Console.ReadLine();
+            Console.Write("Input Summary: ");
+            myBook._summary = Console.ReadLine();
+            myBook.PrintBookUserData();
+
+
+        }
+    }
+
+    public class Book
+    {
+        public string _title;
+        public string _author;
+        public string _summary;
+
+        public Book(string Title, string Author, string Summary) //konstruktor 
+        {
+            _title = Title;
+            _author = Author;
+            _summary = Summary;
+        }
+
+
+        public void PrintBookData()
+        {
+            Console.WriteLine("Book data: {0} {1} {2}",_title, _author, _summary);
+        }
+
+        public void PrintBookUserData()
+        {
+            Console.WriteLine("Book User data: {0} {1} {2}", _title, _author, _summary);
+        }
+    }
+
+
+}
